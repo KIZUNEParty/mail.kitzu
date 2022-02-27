d68cc29a2153539260cc2061e09feae9c26c560d
dethMastery <owner@detzz.in.th>
2022-02-28 03:47:19 +0700

    added: page

---
+++ pages/index.vue
@@ -0,0 +1,24 @@
+<template>
+    <div class="app">
+        <div>
+            <img src="/api/canvas" alt="canvas">
+        </div>
+    </div>
+</template>
+
+<style>
+body {
+    padding: 0;
+    margin: 0;
+    width: 100vw;
+    height: 100vh;
+
+    background: #111;
+}
+
+img {
+    position: fixed;
+    left: calc(50% - 400px);
+    top: calc(50% - 300px);
+}
+</style>
